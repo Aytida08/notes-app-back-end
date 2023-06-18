a5486c265027198481a03352a4fd73d206e8bb9e
Aytida08 <aditya.iya08@gmail.com>
2023-06-18 16:20:24 +0700

    collaboration implemented

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,6 +5,7 @@
     tags,
     created_at,
     updated_at,
+    username,
   }) => ({
     id,
     title,
@@ -12,6 +13,7 @@
     tags,
     createdAt: created_at,
     updatedAt: updated_at,
+    username,
   });
    
   module.exports = { mapDBToModel };
